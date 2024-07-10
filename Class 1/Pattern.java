6e8d875018adbc014acd1e3bc9c534a9e08fda3b
unknown <manishsingh1409@gmail.com>
2024-07-10 22:06:15 +0530

    pattern file

---
+++ Class 1/Pattern.java
@@ -0,0 +1,17 @@
+public class Pattern {
+    public static void main(String[] args) {
+        /*
+        pattern
+
+         ****
+         ***
+         **
+         * 
+         
+         */
+        System.out.println("* * * *");
+        System.out.println("* * *");
+        System.out.println("* *");
+        System.out.println("*");
+    }
+}
--- Class 1/first.java
+++ Class 1/first.java
@@ -8,6 +8,8 @@ public class first {
         System.out.print("Hello World ");
         System.out.print("Hello World ");
         System.out.print("Hello World ");//println gives new line but single print keep all in same line
+        System.out.print("hello world \n hello world \n hello world");
+        // \n is new line character
     }
     
 }
