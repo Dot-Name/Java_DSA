b13f67911dc100ba5eb5c984f1627357a71678df
unknown <manishsingh1409@gmail.com>
2024-07-10 22:49:42 +0530

    additon of input variable pattern files

---
+++ Class 1/input.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class input {
+    public static void main(String[] args) {
+        //input 
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+        System.out.println(input);//it will not store tony stark we have to use nextLine
+        int num = sc.nextInt();
+        System.out.println(num);
+        float price = sc.nextFloat();
+        System.out.println(price);
+        
+    }
+}
---
+++ Class 1/variables.java
@@ -0,0 +1,34 @@
+public class variables {
+    public static void main(String[] args) {
+        /*
+         * 
+         int a = 10;
+         int b = 5;
+         System.out.println(a);
+         System.out.println(b);
+         String name = "Tony Stark";
+         System.out.println(name);
+         a = 50;
+         b = a;
+         System.out.println(a);
+         System.out.println(b);
+         byte num1 = 8;
+         System.out.println(num1);
+         char ch = 'b';
+         System.out.println(ch);
+         boolean bool = true; // true or false only stored in boolean
+         System.out.println(bool);
+         //long
+         //double 
+         //both used for large number and large decimal numbers
+         short n = 240;
+         System.out.println(n);
+         */
+
+        // sum of two number
+        int num1 = 5;
+        int num2 = 10;
+        int sum = num1+ num2;
+        System.err.println("sum of "+num1+" and "+num2+" is " + sum);
+    }
+}
