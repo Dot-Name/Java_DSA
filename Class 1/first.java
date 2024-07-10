06aa7c61b24cee7e0cad85dae6751f14ab17e116
unknown <manishsingh1409@gmail.com>
2024-07-10 21:56:06 +0530

    All set

---
+++ Class 1/first.java
@@ -0,0 +1,14 @@
+
+public class first {
+    //this is class with same name as file name
+    public static void main(String[] args) {
+        //this is function main()
+        System.out.println("Hello World!");//used for output in terminal
+        //and ; (Semi colon) is called statement terminator
+        System.out.print("Hello World ");
+        System.out.print("Hello World ");
+        System.out.print("Hello World ");//println gives new line but single print keep all in same line
+    }
+    
+}
+//boilerplate code
